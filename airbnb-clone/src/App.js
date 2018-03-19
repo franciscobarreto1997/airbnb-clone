32de9cd31e2593588b0033af00dd6b99a87258fa
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2018-03-19 14:01:52 +0000

    added dynamic flats with json parsing and installed google map react

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -4,26 +4,32 @@ import './App.css';
 import Flat from './components/flat';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data
+        });
+      })
+  }
+
   render() {
-
-      const flat = {
-       "name": "Trendy Apt in Buttes Montmartre",
-       "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
-       "price": 200,
-       "priceCurrency": "EUR",
-       "lat": 48.885707,
-       "lng": 2.343543
-      };
-
-      const flats = [flat, flat, flat, flat];
-
     return (
       <div className="app">
         <div className="main">
           <div className="search">
           </div>
           <div className="flats">
-           {flats.map((flat) => {
+           {this.state.flats.map((flat) => {
             return <Flat flat={flat} />
            })}
           </div>
